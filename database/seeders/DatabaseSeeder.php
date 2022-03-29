77c9b137a939f08db7931566598d61af5bf4ade1
ginkgo <alexvid33@gmail.com>
2022-03-29 17:31:57 +0200

    linked and created laravel with DB

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        Event::factory(15)->create();
+    }
+}
