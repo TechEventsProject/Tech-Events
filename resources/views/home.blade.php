82abc1cf4748a9c97e87951a58cd0ab075b36257
ginkgo <alexvid33@gmail.com>
2022-03-31 15:19:18 +0200

    delete action

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,16 @@
 <p>{{$event->name}}</p>
 <img src="{{$event->image}}" alt="">
 
+<form action="{{route('delete', ['id' => $event->id])}}" method="post">
+    @method('delete')
+    @csrf
+    <button type="Submit" onclick="return confirm('Are you sure you want to delete this? {{$event->name}}')">
+        <p>Delete</p>
+    </button>
+</form>
+
+
+
 @endforeach
 
 {{$events->links()}}
