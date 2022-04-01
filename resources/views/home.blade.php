f40186a5febc2e982180713290b88df7b263af4f
ginkgo <alexvid33@gmail.com>
2022-04-01 11:42:00 +0200

    style mod & create first event

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,25 +5,27 @@
 
 
 @foreach ($events as $event)
+<div class="flex justify-center flex-column p-20">
+    <p class="text-center text-2xl">{{$event->name}}</p>
+    <img class="h-full w-full" src="{{$event->image}}" alt="events">
+    <form class="flex justify-end" action="{{route('delete', ['id' => $event->id])}}" method="post">
+        @method('delete')
+        @csrf
+        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" type="submit" onclick="return confirm('Are you sure you want to delete this? {{$event->name}}')">
+            <p>Delete</p>
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <p>Edit</p>
+        </button>
+    </form>
+</div>
 
-<p>{{$event->name}}</p>
-<img src="{{$event->image}}" alt="">
-
-<form action="{{route('delete', ['id' => $event->id])}}" method="post">
-    @method('delete')
-    @csrf
-    <button type="submit" onclick="return confirm('Are you sure you want to delete this? {{$event->name}}')">
-        <p>Delete</p>
-        
-    </button>
-    <button></button>
-</form>
 
 
 
 @endforeach
 
-{{$events->links()}}
+<div class="col-span-2">{{$events->links()}}</div>
 
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Tech Events') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Tech Events') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -59,9 +59,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ route('logout') }}">
+                                        {{ __('My Events') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -76,7 +77,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="grid grid-cols-2 gap-8">
             @yield('content')
         </main>
     </div>
