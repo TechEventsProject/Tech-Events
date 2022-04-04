9eacd12251192d9c47be11df0ee8283a5f6325f1
ginkgo <alexvid33@gmail.com>
2022-04-04 11:31:09 +0200

    linked edit button to edit form and validate

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -9,7 +9,7 @@
         <input type="text" name="name" value="{{$event->name}}">
         <span>Description</span>
         <input type="text" name="description" value="{{$event->description}}">
-        <button type="submit" onclick="return confirm('madafaka {{$event->name}}')">Validate</button>
+        <button type="submit" onclick="return confirm('validate? {{$event->name}}')">Validate</button>
         <button type="button" onclick="{{ route('home') }}">Cancel</button>
     </div>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-
-
 @foreach ($events as $event)
 <div class="flex justify-center flex-column p-20">
     <p class="text-center text-2xl">{{$event->name}}</p>
@@ -14,9 +12,11 @@
         <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" type="submit" onclick="return confirm('Are you sure you want to delete this? {{$event->name}}')">
             <p>Delete</p>
         </button>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            <p>Edit</p>
-        </button>
+        <a href="{{route('edit', ['id' => $event->id])}}" method="post">
+            <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <p>Edit</p>
+            </button>
+        </a>
     </form>
 </div>
 
