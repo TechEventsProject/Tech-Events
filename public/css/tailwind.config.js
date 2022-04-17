5910c4a523db335ee1f0ebd1163b3097930bca8d
itsberriver <jmejiaos713@gmail.com>
2022-04-17 17:40:21 +0200

    styled landing view

---
+++ public/css/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    '*.blade.php',
+  ],
+
+  theme: {
+  
+    colors: {
+      'primary' : '#94DB93',
+      'white':'#FFFDFF',
+      'black':'#000A12',
+
+    }
+
+  },
+  plugins: [],
+}
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/landing.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap" rel="stylesheet"> 
+
+    <!-- Styles -->
+    <link href="{{ asset('css/landing.css') }}" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/176919793f.js" crossorigin="anonymous"></script>
+</head>
+
+<div id="title" class="text-white m-0 .p-0 font-bold relative flex-start p-16" >
+    <h2 id="top" class="m-0 absolute z{2}">TECH</h2>
+    <h2 id="bottom" class="z{1} absolute m-0 top-24 pb-12">TECH</h2>
+</div>
+
+<!-- @foreach ($events as $event) -->
+
+<div class="rounded-full bg-primary m-11 bg-green-800 w-44 h-72 overflow-hidden pt-9 text-white relative mt-56 p-0">
+    <p class="flex justify-center font-bold text-center text-sm pt-4">{{$event->name}}</p>
+    <p class="flex justify-center">{{$event->date_and_time}}</p>
+    <img class="pt-20 w-500 h-500" src="{{$event->image}}" alt="events">
+</div>
+<!-- 
+@endforeach -->
+
+@endsection
