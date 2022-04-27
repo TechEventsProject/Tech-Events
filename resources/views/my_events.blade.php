7a194e4046f0269fd9b390d534e738b5b41f05c7
ginkgo <alexvid33@gmail.com>
2022-04-27 14:52:24 +0200

    designed my_events view

--- resources/views/my_events.blade.php
+++ resources/views/my_events.blade.php
@@ -5,9 +5,10 @@
         <h2 id="top" class="m-0 absolute z-10 text-4xl md:text-7xl">MY EVENTS</h2>
         <h2 id="bottom" class="absolute z-0 mt-3 text-4xl md:text-7xl">MY EVENTS</h2>
     </div>
+    <section class="grid grid-cols-2 col-span-2 gap-20 flex justify-items-center p-9 md:grid-cols-3 lg:grid-cols-4">
     @foreach ($myEventUser as $event) 
     <div id="backgroundImage" class="font-[Montserrat] rounded-[38px] bg-cover bg-center bg-[url('{{$event->image}}')]
-    flex flex-col text-[#FFFDFF] h-44 w-40 items-center text-center justify-center md:h-64 md:w-60">
+    flex flex-col justify-between items-center text-[#FFFDFF] h-44 w-40 text-center md:h-64 md:w-60">
         <div class="relative -bottom-[14px]">
             <p class='text-sm font-semibold relative -bottom-[5px]'>{{ date('d/m/Y', strtotime($event->date_and_time)) }}</p>
             <p class="text-xl font-semibold">{{$event->speaker}}</p>
@@ -15,9 +16,10 @@
         <div class="relative flex justify-center items-center bg-[#69C4A0] rounded-3xl relative -bottom-[17px] h-[30px] w-[144px] ">
             <a class="text-sm font-semibold align-middle leading-[12px]" href="{{route('show', ['id' => $event->id])}}">{{$event->name}}</a>
         </div>
-        <a href="{{route('unsubscribe', ['id'=>$event->id])}}" class="">
+        <a href="{{route('unsubscribe', ['id'=>$event->id])}}">
             <button type="submit" class="h-9 w-32 rounded-3xl bg-white font-['Montserrat'] font-bold" onclick="return confirm('Do you want to unsubscribe from {{$event->name}}?')">Unsubscribe</button>
         </a>
     </div>
     @endforeach
+    </section>
 @endsection
