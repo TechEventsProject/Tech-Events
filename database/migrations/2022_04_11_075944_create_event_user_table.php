4690aa53e25065399860bdf12dcab4316973fa00
Adrian Pelayo <niniohhns@gmail.com>
2022-04-28 12:33:51 +0200

    delete where foreign keys are up

--- database/migrations/2022_04_11_075944_create_event_user_table.php
+++ database/migrations/2022_04_11_075944_create_event_user_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->id();
 
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
             $table->unsignedBigInteger('event_id');
-            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');;
 
             $table->timestamps();
         });
